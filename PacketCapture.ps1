f5519b5ced08a287b7744b14ab7bb6185a1acf39
SilverSpectre <35970087+SilverSpectre@users.noreply.github.com>
2021-01-22 08:05:25 -0700

    End of work 1-21-21

--- PacketCapture.ps1
+++ PacketCapture.ps1
@@ -1,1 +1,7 @@
-New-NetEventSession -name "test" -CaptureMode SaveToFile -LocalFilePath "C:\NetEvents\test" -CimSession 
+[string]$session = Read-Host -Prompt 'Please enter a filename'
+$targets = Read-Host -Prompt 'Please enter a target computer name'
+$targetIPs = "192.168.80.10" #Variable for target's IPs, if necessary. May delete in future patch.
+New-CimSession -ComputerName $targets
+New-NetEventSession -Name $session -CaptureMode SaveToFile -LocalFilePath "C:\NetEvents\$session"
+Add-NetEventProvider  -SessionName $session -Name "Microsoft-Windows-TCPIP" -Level "5"
+Add-NetEventPacketCaptureProvider -SessionName $session -CaptureType "Switch" -Level "5" -IpAddresses $targetIPs -IpProtocols "2"
