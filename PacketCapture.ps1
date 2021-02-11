ce1af60a93686e5aa8025dec6be2a15a89c4aaad
SilverSpectre <35970087+SilverSpectre@users.noreply.github.com>
2021-02-11 11:23:24 -0700

    Add files via upload
    Successfully grabs an .etl file

--- PacketCapture.ps1
+++ PacketCapture.ps1
@@ -1,7 +1,22 @@
+#User input
 [string]$session = Read-Host -Prompt 'Please enter a filename'
 $targets = Read-Host -Prompt 'Please enter a target computer name'
-$targetIPs = "192.168.80.10" #Variable for target's IPs, if necessary. May delete in future patch.
-New-CimSession -ComputerName $targets
-New-NetEventSession -Name $session -CaptureMode SaveToFile -LocalFilePath "C:\NetEvents\$session"
+$targetIPs = Read-Host -Prompt 'Please enter a target IP'
+
+#Hopeful stretch goal to link 2 specific computers
+    #$cred = Get-Credential
+    #New-CimSession - Name $session -ComputerName $targets -Credential $cred
+    #New-NetEventSession -Name $session -CaptureMode SaveToFile -LocalFilePath "C:\NetEvents\$session"
+    #Add-NetEventProvider  -SessionName $session -Name "Microsoft-Windows-TCPIP" -Level "5"
+    #Add-NetEventPacketCaptureProvider -SessionName $session -CaptureType "Switch" -Level "5" -IpAddresses $targetIPs -IpProtocols "2"
+    
+#Default NetTrace location
+    #C:\Windows\system32\config\systemprofile\AppData\Local\NetEventTrace.etl
+
+#Current runnning script
+New-NetEventSession -Name $session
 Add-NetEventProvider  -SessionName $session -Name "Microsoft-Windows-TCPIP" -Level "5"
 Add-NetEventPacketCaptureProvider -SessionName $session -CaptureType "Switch" -Level "5" -IpAddresses $targetIPs -IpProtocols "2"
+Start-NetEventSession
+ping $targets
+Stop-NetEventSession
