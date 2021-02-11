0c634f1f9460d16dfa6b732f4a44ee446bfeb9e7
SilverSpectre <35970087+SilverSpectre@users.noreply.github.com>
2021-02-11 13:16:43 -0700

    Successful run of the Script
    Cleaned up syntax, and added comments to denote section of the script

--- PacketCapture.ps1
+++ PacketCapture.ps1
@@ -1,8 +1,3 @@
-#User input
-[string]$session = Read-Host -Prompt 'Please enter a filename'
-$targets = Read-Host -Prompt 'Please enter a target computer name'
-$targetIPs = Read-Host -Prompt 'Please enter a target IP'
-
 #Hopeful stretch goal to link 2 specific computers
     #$cred = Get-Credential
     #New-CimSession - Name $session -ComputerName $targets -Credential $cred
@@ -14,9 +9,15 @@ $targetIPs = Read-Host -Prompt 'Please enter a target IP'
     #C:\Windows\system32\config\systemprofile\AppData\Local\NetEventTrace.etl
 
 #Current runnning script
+    #User input
+[string]$session = Read-Host -Prompt 'Please enter a filename'
+$targets = Read-Host -Prompt 'Please enter a target computer name'
+$targetIPs = Read-Host -Prompt 'Please enter a target IP'
+
 New-NetEventSession -Name $session
 Add-NetEventProvider  -SessionName $session -Name "Microsoft-Windows-TCPIP" -Level "5"
 Add-NetEventPacketCaptureProvider -SessionName $session -CaptureType "Switch" -Level "5" -IpAddresses $targetIPs -IpProtocols "2"
-Start-NetEventSession
+Start-NetEventSession -Name $session
 ping $targets
-Stop-NetEventSession
+Stop-NetEventSession -Name $session
+Remove-NetEventSession -Name $session
