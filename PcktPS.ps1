2f6b3556ddad24793da2dd193178de91cdcbd188
SilverSpectre <35970087+SilverSpectre@users.noreply.github.com>
2021-05-18 17:02:07 -0600

    Parsing Python solved

--- PcktPS.ps1
+++ PcktPS.ps1
@@ -1,20 +1,18 @@
-#Hopeful stretch goal to link 2 specific computers
-    #$cred = Get-Credential
-    #New-CimSession - Name $name -ComputerName $targets -Credential $cred
-    #New-NetEventSession -Name $name -CaptureMode SaveToFile -LocalFilePath "C:\NetEvents\$name"
-    #Add-NetEventProvider  -SessionName $name -Name "Microsoft-Windows-TCPIP" -Level "5"
-    #Add-NetEventPacketCaptureProvider -SessionName $name -CaptureType "Switch" -Level "5" -IpAddresses $ip -IpProtocols "2"
-    
-#Default NetTrace location
-    #C:\Windows\system32\config\systemprofile\AppData\Local\NetEventTrace.etl
-
-#Current runnning script
-    #User input
-param($name, $ip)
-New-NetEventSession -Name $name
+$cred = Get-Credential
+Get-Content .\options.txt | Foreach-Object {
+    $var = $_.Split('=')
+    New-Variable -Name $var[0] -Value $var[1]
+}
+Remove-Item .\options.txt
+$cim = New-CimSession -Name $name -ComputerName $target -Authentication Negotiate -Credential $cred
+New-NetEventSession -Name $name -CaptureMode SaveToFile -LocalFilePath "C:\NetEvents\$name" -CimSession $cim
 Add-NetEventProvider  -SessionName $name -Name "Microsoft-Windows-TCPIP" -Level "5"
 Add-NetEventPacketCaptureProvider -SessionName $name -CaptureType "Switch" -Level "5" -IpAddresses $ip -IpProtocols "2"
 Start-NetEventSession -Name $name
 ping $ip
 Stop-NetEventSession -Name $name
 Remove-NetEventSession -Name $name
+Remove-CimSession -name $cim
+
+#Default NetTrace location
+#C:\Windows\system32\config\systemprofile\AppData\Local\NetEventTrace.etl
--- PcktPY.py
+++ PcktPY.py
@@ -1,28 +1,39 @@
 from tkinter import *
 import subprocess
-import time
 
 root = Tk()
 filename = Entry()
+target = Entry()
 ip = Entry()
 
 def Script():
-    confirm = Label(root, text="Getting Packets...")
-    confirm.pack()
-    subprocess.Popen(".\\PcktPS.ps1 -name " + filename.get() + " -ip " + ip.get(), shell=True)
-    # import data
-    
-def Pack():
-    title.pack()
-    fil.pack()
+    options = "name=" + filename.get() + "\ntarget=" + target.get() + "\nip=" + ip.get()
+    Add(options)
+    Label(root, text="Getting Packets...").pack()
+    subprocess.Popen('powershell', shell=True)
+    subprocess.run('.\PcktPS.ps1', shell=True)
+    subprocess.Popen('exit', shell=True)
+
+def Add(content):
+    data = open("options.txt", "w+")
+    data.write(content)
+    data.close()
+
+def Package():
+    Label(root, text="Packet Dissector v.2\nBy Gavin Weber", padx="50").pack()
+    Label(root, text="Filename").pack()
     filename.pack()
-    IP.pack()
+    Label(root, text="Computer Name").pack()
+    target.pack()
+    Label(root, text="IP Address").pack()
     ip.pack()
-    getPacket.pack()
+    Button(root, text="Get Packets", padx="10",
+           pady="10", command=Script).pack()
     root.mainloop()
 
-title = Label(root, text="Packet Dissector v.1\nBy Gavin Weber", padx="50")
-fil = Label(root, text="Filename")
-IP = Label(root, text="IP")
-getPacket = Button(root, text="Get Packet", padx="10", pady="10", command=Script)
-Pack()
+def Import():
+    # importing and returning etl file.
+    print("Currently not displaying data")
+
+Package()
+Import()
